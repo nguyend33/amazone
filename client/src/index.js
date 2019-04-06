85eb6bf3d094d993ff0caf4bfdc4863a27db0dc8
Katelyn Williamson <katelynrwilliamson@gmail.com>
2019-04-06 11:43:22 -0600

    added devise-axios to fixe logout etc

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
 import { AuthProvider, } from "./components/providers/AuthProvider";
 import 'semantic-ui-css/semantic.min.css';
+import { initMiddleware, } from 'devise-axios';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
